6ea4086c2cf98dbf0acbe4a20fb85fc41f6732d0
cpor3 <85139003+cpor3@users.noreply.github.com>
2021-09-23 15:36:40 -0300

    change description

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>REACT</h1>
-      <h2>mounted on Heroku with codesandobox</h2>
+      <h2>Mounted on Heroku with codesandobox</h2>
     </div>
   );
 }

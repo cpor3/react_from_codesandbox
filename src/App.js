e04ff59f63164426875a20bc83e83afed7cd2322
cpor3 <85139003+cpor3@users.noreply.github.com>
2021-09-23 15:33:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>REACT</h1>
+      <h2>mounted on Heroku with codesandobox</h2>
+    </div>
+  );
+}
